04b615956398ab6ac8513e326d6e4615489d7fd8
jayunit100 <jay@apache.org>
2017-11-17 19:38:50 -0500

    Configurable xpontential bucket functionality and nslookup

--- cmd/sidecar/service_scanner.go
+++ cmd/sidecar/service_scanner.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"os"
 	"strings"
@@ -22,20 +23,28 @@ func init() {
 	cfg = ViperLoad()
 	viper.WatchConfig()
 
-	svcNames := func() []string {
-		return []string{"service", "port"}
-	}
-	serviceCheck = prometheus.NewHistogramVec(
+	curlCheck = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Namespace: "sidecar",
 			Subsystem: "metrics",
-			Name:      "dns_lookup",
-			Help:      "The current lookup time for a service.",
-			Buckets:   []float64{1, 5, 10},
+			Name:      "curl",
+			Help:      "The current CURL time for a service in milliseconds.",
+			Buckets:   prometheus.ExponentialBuckets(1, 2, cfg.Buckets),
 		},
-		svcNames())
+		[]string{"service", "port", "status"})
 
-	prometheus.MustRegister(serviceCheck)
+	nsLookup = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: "sidecar",
+			Subsystem: "metrics",
+			Name:      "ns_lookup",
+			Help:      "The current NS LOOKUP time for a service. Labelled with IP to detect schizophrenic resolution scenarios.",
+			Buckets:   prometheus.ExponentialBuckets(1, 2, cfg.Buckets),
+		},
+		[]string{"service", "numIP"})
+
+	prometheus.MustRegister(curlCheck)
+	prometheus.MustRegister(nsLookup)
 
 	// This allows someone to go into the container and change the curl endpoints.
 	// for like realtime debugging.
@@ -47,27 +56,44 @@ func init() {
 	go func() {
 		for {
 			for svc, p := range cfg.Services {
-				milliseconds, err := cfg.Curl(svc, p)
+				milliseconds, status, err := cfg.Curl(svc, p)
 				if err != nil {
-					serviceCheck.WithLabelValues(svc, fmt.Sprintf("%v", p)).Observe(float64(9999999))
+					curlCheck.WithLabelValues(svc, fmt.Sprintf("%v", p), status).Observe(float64(9999999))
 				} else {
-					serviceCheck.WithLabelValues(svc, fmt.Sprintf("%v", p)).Observe(float64(milliseconds))
+					curlCheck.WithLabelValues(svc, fmt.Sprintf("%v", p), status).Observe(float64(milliseconds))
 				}
 			}
 			time.Sleep(5 * time.Second)
 		}
 	}()
-
+	// Separate go thread for each metric, to avoid any forced correlation that might come from
+	// the n+1th test being less efficient then the nth.  i.e. maybe nslookup the first time makes
+	// curl faster the second time.
+	go func() {
+		for {
+			for svc, _ := range cfg.Services {
+				milliseconds, ips, err := cfg.NSLookupIP(svc)
+				if err != nil {
+					nsLookup.WithLabelValues(svc, fmt.Sprintf("%v", len(ips))).Observe(float64(9999999))
+				} else {
+					nsLookup.WithLabelValues(svc, fmt.Sprintf("%v", len(ips))).Observe(float64(milliseconds))
+				}
+			}
+			time.Sleep(5 * time.Second)
+		}
+	}()
 }
 
 type Config struct {
-	Services   map[string]int
-	SvcTimeout int
+	Services   map[string]int // the map of service:port that sidecar scans over
+	SvcTimeout int            // how long to wait for timeout when curling endpoints.
+	Buckets    int            // number of exponentials for nslookup and so on .  easier to read if keep small unless debugging.
 }
 
 var (
-	serviceCheck *prometheus.HistogramVec
-	cfg          *Config
+	curlCheck *prometheus.HistogramVec
+	nsLookup  *prometheus.HistogramVec
+	cfg       *Config
 )
 
 func main() {
@@ -84,7 +110,8 @@ func main() {
 
 }
 
-func (cfg *Config) Curl(svc string, port int) (int64, error) {
+// Curl returns time to curl, status, and any error.
+func (cfg *Config) Curl(svc string, port int) (int64, string, error) {
 	SvcTimeout := time.Duration(cfg.SvcTimeout) * time.Second
 	client := http.Client{
 		Timeout: SvcTimeout,
@@ -92,9 +119,21 @@ func (cfg *Config) Curl(svc string, port int) (int64, error) {
 	start := time.Now()
 	url := strings.Join([]string{"http://" + svc, fmt.Sprintf("%v", port)}, ":")
 	resp, err := client.Get(url)
-	log.Info(fmt.Sprintf("Response from %v was %v : %v", url, resp, err))
+	status := ""
+	if resp != nil {
+		status = resp.Status
+	}
+	log.Info(fmt.Sprintf("CURL Response from %v was %v : %v", url, status, err))
 	elapsed := int64(time.Since(start) / time.Millisecond)
-	return elapsed, err
+	return elapsed, status, err
+}
+
+func (cfg *Config) NSLookupIP(svc string) (int64, []net.IP, error) {
+	start := time.Now()
+	ips, err := net.LookupIP(svc)
+	log.Info(fmt.Sprintf("LOOKUP Response from %v was ips = %v : %v", svc, len(ips), err))
+	elapsed := int64(time.Since(start) / time.Millisecond)
+	return elapsed, ips, err
 }
 
 func (c *Config) url(s string) string {
@@ -114,7 +153,8 @@ func ViperLoad() *Config {
 		  "solr":0,
 		  "documentation": 0
 		},
-		"svcTimeout":10
+		"svcTimeout":10,
+		"buckets":3
 	}`
 	if v, ok := os.LookupEnv("ENV_CONFIG_JSON"); ok {
 		sidecarTargets = v
