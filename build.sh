a2266449662d90537939bccad99a41b6107dd454
jay vyas <jayunit100@pcw-pjadovich-1.bds-ad.lc>
2017-12-12 15:14:12 -0500

    working example of a kubernetes based openshift client

--- build.sh
+++ build.sh
@@ -15,6 +15,6 @@ rm main
 # This will put the 'sidecar' binary into your GOPATH.
 rm $GOPATH/bin/service_scanner
 export GOBIN=$GOPATH/bin
-go install ./cmd/sidecar/service_scanner.go
+go install ./cmd/sidecar/worker.go
 
-$GOPATH/bin/service_scanner
+$GOPATH/bin/worker --kubeconfig=/Users/jayunit100/.kube/config --master=https://34.227.56.110.xip.io:8443
